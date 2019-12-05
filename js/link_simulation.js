62f28eb8a970e77e0764a17b219397935b46944a
akirayzh <664753092@qq.com>
2019-12-05 19:19:39 +0800

    search refined

--- js/link_simulation.js
+++ js/link_simulation.js
@@ -1,24 +1,11 @@
 window.onload = function () {
-    
-    if(document.querySelector('#carousel')){
+
+    if (document.querySelector('#carousel')) {
         new Carousel('#carousel');
     }
-    
-    
-    /////////////////////
-    var oSearch_bar = document.querySelector('#search_bar_s');
-    var oTags = document.querySelector('#tags');
-    var oMatches = document.querySelector('#match');
 
-    oSearch_bar.onfocus = function () {
-        oTags.style.display = 'none';
-        oMatches.style.display = 'block';
-    }
 
-    oSearch_bar.onblur = function () {
-        oTags.style.display = 'block';
-        oMatches.style.display = 'none';
-    }
+
 
     //////////
     var btn_login = document.querySelector('#login');
@@ -70,9 +57,9 @@ window.onload = function () {
                 aLi[i].children[2].style.transform = 'scaleY(0)';
                 aLi[i].children[2].style.height = 0;
                 aLi[i].children[1].style.transform = 'rotate(0)';
-                
+
             }
-            if(option.parentNode.open){
+            if (option.parentNode.open) {
                 option.parentNode.open = false;
                 return;
             }
@@ -87,4 +74,130 @@ window.onload = function () {
 
 
     }
+
+
+    /////////////////////////
+    var json = {
+        txt1: "melon真帅melon真帅melon真帅melon真帅melon真帅melon真帅melon真帅",
+        txt2: "roger真帅roger真帅roger真帅roger真帅roger真帅roger真帅",
+        txt3: "鸡你太美鸡你太美鸡你太美鸡你太美鸡你太美",
+        txt4: "GIMC VS 易车，汽车电商“风口”上的2种姿势2014年11月易车市值接近汽车之家时，正值公布上季度财报，而在2014年Q3业绩易车网净1.86亿，超过汽车1.71亿。看来美国投资人是完全按净利润、自由现金流为这两家公司估值，对汽车电商..."
+
+    }
+
+    var oSearch_bar = document.querySelector('#search_bar_s');
+    var oTags = document.querySelector('#tags');
+    var oMatches = document.querySelector('#match');
+    // var keyword = null;
+
+
+
+
+    oSearch_bar.onkeyup = function () {
+        //When input length is 0, show the tags
+        if (oSearch_bar.value.length == 0) {
+            // alert();
+            oTags.style.display = 'block';
+            oMatches.style.display = 'none';
+            return;
+        }
+
+        oTags.style.display = 'none';
+        oMatches.style.display = 'block';
+        keywords = oSearch_bar.value.split(' ');
+
+
+        // clear show record everytime the user types something
+
+        oMatches.innerHTML = "";
+
+
+        // sort keywords by length and remove repeated children
+        keywords = sortKeywords(keywords);
+
+        function sortKeywords(keywords) {
+            keywords.sort(function (n1, n2) {
+                return n1.length - n2.length;
+            });
+            // remove blank children
+            for (i = 0; i < keywords.length; i++) {
+                if (keywords[i].length == 0) {
+                    keywords.splice(i, 1);
+                    i = i - 1;
+                }
+            }
+
+            // remove repeated children
+            var keywords_nr = [];
+            for (i = 0; i < keywords.length; i++) {
+                if (!keywords_nr.includes(keywords[i])) {
+                    keywords_nr.push(keywords[i]);
+                }
+            }
+            //reverse the array sequence (for Practice )
+            keywords_nr.reverse();
+            return keywords_nr;
+        }
+
+
+
+
+        // show the matches
+
+
+        for (i in json) {
+
+            // var text_o = json[i];
+            var text = json[i];
+            var oLi = document.createElement('li');
+            var oH3 = document.createElement('h3');
+            var match = true;
+            
+
+            // juge if the text contains all the keywords
+            for (j = 0; j < keywords.length; j++) {
+                if (json[i].toLowerCase().search(keywords[j].toLowerCase()) == -1) {
+                    match = false;
+                }
+
+            }
+            console.log(keywords)
+
+            if (match == true) {
+
+                for (j = 0; j < keywords.length; j++) {
+
+                    var _text = text;
+                    var _keyword = keywords[j];
+                    var reg = new RegExp('(?<!\<|\<\/)'+keywords[j] + '(?!\>)', 'gi');
+
+                    _keyword = _text.substr(text.toLowerCase().indexOf(keywords[j].toLowerCase()), keywords[j].length);
+                    console.log(keywords[j]);
+                    text = _text.replace(reg, '<m>' + _keyword + '</m>');
+
+                    console.log(reg);
+
+
+                }
+
+                oH3.innerHTML = text;
+                oLi.appendChild(oH3);
+                oLi.classList.add('match');
+                oMatches.appendChild(oLi);
+            }
+
+
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+
 }
